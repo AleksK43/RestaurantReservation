039aa99db2f1fa40eb674ef22910f3c486140912
AleksK43 <117122687+AleksK43@users.noreply.github.com>
2024-01-08 22:34:38 +0100

    Adding Login Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './Pages/home-page/home-page.component';
 import { MainPageGridComponent } from './Components/main-page-grid/main-page-grid.component';
 import { AboutUsComponent } from './Components/about-us/about-us.component';
+import { LoginComponent } from './Components/login/login.component';
 
 const routes: Routes = [
   {path: 'HomePage', component:HomePageComponent},
   { path: 'MainGrid', component: MainPageGridComponent},
   { path: 'AboutUs', component: AboutUsComponent},
+  {path: 'login', component: LoginComponent},
   { path: '', component: HomePageComponent, pathMatch: 'full' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './Pages/home-page/home-page.component';
 import { MainPageGridComponent } from './Components/main-page-grid/main-page-grid.component';
 import { AboutUsComponent } from './Components/about-us/about-us.component';
 import { ContactComponent } from './Components/contact/contact.component';
+import { LoginComponent } from './Components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,8 @@ import { ContactComponent } from './Components/contact/contact.component';
     HomePageComponent,
     MainPageGridComponent,
     AboutUsComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
